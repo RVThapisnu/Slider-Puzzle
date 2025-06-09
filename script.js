f1e96955c107960217efb07202aeb127a4c49965
RVThapisnu <rvthapisnu@gmail.com>
2025-06-09 18:38:34 +0530

    Added functions to determine Winning Phase

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const gparent = document.getElementsByTagName('gparent')[0];
 const gchilds = document.getElementsByTagName('gchild');
 
-const rows = 3;
-const cols = 3;
+const rows = 2;
+const cols = 2;
 
 // Blank piece Index
 const b_index = 0;
 const area_label = "p";
+
+let wrong_pieces = 0;
 
 const pos = {
     "vert" : ["top","center","bottom"],
@@ -37,11 +39,33 @@ for (let i=0,col=0;i < gchilds.length;i ++) {
     let index = shuffled_order[i];
     gchilds[index].style.gridArea = toArea(i + 1); // Area starts from 1
 
+    // Count Wrong Pieces
+    if(gchilds[i] != gchilds[index]) {
+	wrong_pieces ++;
+    }
+
     // Set except for 1st child
     if(i != b_index) {
 	gchilds[i].style.backgroundPosition = `${pos['vert'][col]} ${pos['horz'][i % cols]}`;
 	gchilds[i].addEventListener('click',function() {
-	    swapper(this);
+	    const t = this; // Target Piece
+	    const b = gchilds[b_index]; // Blank Piece
+
+	    const t_area = t.style;
+	    const b_area = b.style;
+
+	    const t_pos = toIndex(t_area.gridArea);
+	    const b_pos = toIndex(b_area.gridArea);
+
+	    let a = swapper(t_area,b_area,t_pos,b_pos);
+	    checkPos(t,t_pos);
+	    checkPos(b,b_pos);
+
+	    // Win Event
+	    if(wrong_pieces == 0) {
+		// Due To EventLoop in JS
+		setTimeout(() => { alert('You have won the game') },0);
+	    }
 	});
     }
 }
@@ -49,22 +73,30 @@ for (let i=0,col=0;i < gchilds.length;i ++) {
 // Core Functions
 
 // Swapping Pieces / Tiles
-function swapper(obj) {
-    const t_area = obj.style;
-    const b_area = gchilds[b_index].style;
-
-
-    const t_pos = toIndex(t_area.gridArea);
-    const b_pos = toIndex(b_area.gridArea);
-
+function swapper(t_area,b_area,t_pos,b_pos) {
     const isVertical = checkVertical(t_pos,b_pos);
     const isHorizontal = checkHorizontal(t_pos,b_pos);
 
     if (isVertical || isHorizontal) {
-	const tmp = t_area.gridArea;
-        t_area.gridArea = b_area.gridArea;
-	b_area.gridArea = tmp;
+	[t_area.gridArea,b_area.gridArea] = [b_area.gridArea,t_area.gridArea];
     }
+
+    return true;
+}
+
+// Check whether piece is in the Correct Position and Update
+function checkPos(piece,prev_Pos) {
+    const curr_Pos = toIndex(piece.style.gridArea);
+    const isPreviousWrong = gchilds[prev_Pos - 1] != piece;
+    const isCurrentWrong = gchilds[curr_Pos - 1] != piece;
+
+    if(!isCurrentWrong) {
+	wrong_pieces --;
+    }
+    else if(isCurrentWrong && !isPreviousWrong) {
+	wrong_pieces ++;
+    }
+    console.log("Wrong =",wrong_pieces);
 }
 
 // Check Vertical Positions
